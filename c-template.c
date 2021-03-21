b0f8ad11271b72ad44724212ccfa2017cfcc779c
Jamie <jamie.bainbridge@gmail.com>
2021-03-21 15:37:19 +1000

    Create c-template.c

---
+++ c-template.c
@@ -0,0 +1,19 @@
+/* copyright/licensing */
+
+/* includes */
+
+/* defines and macros */
+
+/* external declarations */
+
+/* typedefs, structs, enums, etc */
+
+/* global variable declarations */
+
+/* function prototypes */
+
+/* if main() - command-line parsing */
+
+/* function declarations */
+
+/* based on: https://opensource.com/article/19/5/how-write-good-c-main-function */
