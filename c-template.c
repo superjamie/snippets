61742e7b47f1bc16dd01eed76bd88eaf253cab3b
Jamie <jamie.bainbridge@gmail.com>
2021-07-15 07:38:39 +1000

    Update c-template.c

--- c-template.c
+++ c-template.c
@@ -1,19 +1,37 @@
-/* copyright/licensing */
+/**
+ * reference
+ * https://opensource.com/article/19/5/how-write-good-c-main-function
+ * https://google.github.io/styleguide/cppguide.html#Names_and_Order_of_Includes
+ * https://matt.sh/howto-c
+ */
+
+/**
+ * header
+ */
+
+/* include guard */
+#pragma once
+
+/* includes
+   - system, eg <stdio.h>
+   - libraries, eg: <SDL2/SDL2.h>
+   - local project, eg: "myfile.h"
+ */
+
+/* macros */
+
+/* types - structs, enums, etc */
+
+/* variable declarations */
+
+/* prototypes */
+
+/**
+ * code
+ */
 
 /* includes */
 
-/* defines and macros */
-
-/* external declarations */
-
-/* typedefs, structs, enums, etc */
-
-/* global variable declarations */
-
-/* function prototypes */
-
 /* if main() - command-line parsing */
 
-/* function declarations */
-
-/* based on: https://opensource.com/article/19/5/how-write-good-c-main-function */
+/* functions */
