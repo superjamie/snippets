e56bad17f8e168decbbb74f1572f8c79821b0f84
Jamie <jamie.bainbridge@gmail.com>
2022-06-04 15:16:50 +1000

    Create guake-palettes.py

---
+++ guake-palettes.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8; -*-
+"""
+Copyright (C) 2007-2013 Guake authors
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301 USA
+"""
+
+# index 00: Host
+# index 01: Syntax string
+# index 02: Command
+# index 03: Command color 2
+# index 04: Path
+# index 05: Syntax var
+# index 06: Promp
+# index 07:
+# index 08:
+# index 09: Command error
+# index 10: Exec
+# index 11:
+# index 12: Folder
+# index 13:
+# index 14:
+# index 15:
+# index 16: Background Color
+# index 17: Foreground Color
+
+PALETTES = {
+    "Solarized Dark": (
+        "#070736364242:#DCDC32322F2F:#858599990000:#B5B589890000:#26268B8BD2D2:"
+        "#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:"
+        "#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:"
+        "#FDFDF6F6E3E3:#EEEEE8E8D5D5:#00002B2B3636"
+    )
+}
